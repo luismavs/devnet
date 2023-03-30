f787a19c2d0c7f1d385b0c10ffb355b730b51b91
Luís Seabra <luis.seabra@innowave.tech>
2023-03-30 15:41:33 +0100

    proper packaging and build wheel

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-from setuptools import find_namespace_packages, setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name="devnet",
-    version="0.3.1",
+    version="0.3.2",
     description="Unofficial pytorch implementation of deviation network for table data.",
     author="Yuji Kamiya",
     author_email="y.kamiya0@gmail.com",
     maintainer="Luís Seabra",
-    author_email="luismavseabra@gmail.com",
+    maintainer_email="luismavseabra@gmail.com",
     license="MIT",
     packages=find_packages(where='src'),
-    # packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=["pandas", "torch", 'scikit-learn', 'logzero','hydra-core'],
     zip_safe=False,
 )
-
-# from Cython.Build import cythonize
-# ext_modules=cythonize("anomalydetector/inwkitsune/feature_extractor/after_image_cython.pyx"),
